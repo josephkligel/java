7805a207b008746faffdaefd681a2e454120424e
Joseph Kligel <jkligel@gmail.com>
2023-04-12 03:17:11 -0500

    Java OOP

---
+++ OOP/fundamentals/ProjectOne/src/GreeterTest.java
@@ -0,0 +1,10 @@
+
+public class GreeterTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.greet());
+	}
+
+}
